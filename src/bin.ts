0381459ccef9eeb65ab847fc16720e6b718ae81b
Gustavo Barragan <dev@droni.co>
2024-12-28 12:16:37 -0500

    fix: group function copy files

--- src/bin.ts
+++ src/bin.ts
@@ -4,30 +4,31 @@ import path from 'node:path'
 console.log('hola mundo')
 console.log('Procces Arguments: ', process.argv)
 
-const folderName = path.resolve('.');
-
-try {
-  if (!fs.existsSync(folderName)) {
-    fs.mkdirSync(folderName);
-  }
-} catch (err) {
-  console.error(err);
-}
-
-// copy template files from start folder to new folder
-const templateFolder = import.meta.dirname+'/../start';
-const files = fs.readdirSync(templateFolder);
-files.forEach(file => {
-  fs.copyFileSync(`${templateFolder}/${file}`, `${folderName}/${file}`);
-});
-
-
 // ask for name of project
 console.log('What is the name of your project?')
 // read input
 process.stdin.on('data', (data) => {
   const name = data.toString().trim()
-  console.log(name)
+  copyFiles(name)
+  console.log('Project created successfully')
   process.exit()
 })
 
+const copyFiles = (projectName:string) => {
+  const folderName = path.resolve('.')+'/'+projectName;
+
+  try {
+    if (!fs.existsSync(folderName)) {
+      fs.mkdirSync(folderName);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+
+  // copy template files from start folder to new folder
+  const templateFolder = import.meta.dirname+'/../start';
+  const files = fs.readdirSync(templateFolder);
+  files.forEach(file => {
+    fs.copyFileSync(`${templateFolder}/${file}`, `${folderName}/${file}`);
+  });
+}
